2e8a3bbf7a0b110a289ca3bbe8f00215a691df2a
Dror Cohen <dror@cogniteam.com>
2022-06-08 17:57:26 +0300

    fix user list + update

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../Models/User')
 
 const readUsers = async (req,res) =>{
     try {
-        const users = User.find();
+        const users = await User.find();
         res.status(200).json(users);
     } catch (err) {
         res.status(404).json({error: err.message});
@@ -18,15 +18,19 @@ const createUser = async (req,res) => {
         res.status(404).json({ message:error });
     }
 }
+//TODO: add authentication for request
 const updateUser = async (req,res) => {
+    console.log('start update');
+    console.log(`req.body: ${req.body.toString()}`);
     const {id} = req.params;
     const {firstName, lastName, creationDate, isAdmin, allPostIDs, email, password, profilePicture, isDeleted} = req.body;
     if(!mongoose.isValidObjectId(id))
         return res.status(404).send(`the id ${id} is not valid`);
     const user = {_id:id, firstName, lastName, creationDate, isAdmin, allPostIDs, email, password, profilePicture, isDeleted};
-    await User.findByIdAndUpdate(user);
+    await User.findByIdAndUpdate(id,req.body);
     res.json(user);
 }
+//TODO: add authentication for request
 const deleteUser = async (req,res) => {
     const {id} = req.params;
     if(!mongoose.isValidObjectId(id))
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -4,7 +4,7 @@ const usersRouter = express.Router();
 
 usersRouter.get('/',userController.readUsers);
 usersRouter.post('/',userController.createUser);
-usersRouter.patch('/',userController.updateUser);
+usersRouter.patch('/:id',userController.updateUser);
 usersRouter.delete('/:id',userController.deleteUser);
 
 module.exports = usersRouter;
