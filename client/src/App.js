286e1cf30ebde9915f58745b68d255590a375266
Dror Cohen <dror@cogniteam.com>
2022-06-18 16:32:02 +0300

    add initial sign up component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, BrowserRouter} from 'react-router-dom';
 
 import Login from './components/Login';
 import Home from './pages/Home';
+import {SignUp} from "./components/sign-up/SignUp";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
