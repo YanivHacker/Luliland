8a942ae26827a7d130bc4751466cca4d84811fce
Dror Cohen <dror@cogniteam.com>
2022-06-20 19:46:26 +0300

    wip

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from "react"
 import {signUp} from "../../services/UserService";
 
-export function SignUp(){
+export const SignUp = ()=>{
     const firstName = useRef()
     const lastName = useRef()
     const email = useRef()
