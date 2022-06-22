ab518c2b75aaec31e95477edea8e148e13d33b61
Dror Cohen <dror@cogniteam.com>
2022-06-22 19:34:24 +0300

    add image up-loader component
    still should to design it

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {LoadImage} from "../components/ImageUplaoder/ImageUploader";
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
+        <LoadImage/>
     </>
   )
 }
