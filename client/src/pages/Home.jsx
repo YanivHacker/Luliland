763e2bcb85a1f8dc6e48dc0575ab25c0ddca835d
Dror Cohen <dror@cogniteam.com>
2022-06-22 18:06:17 +0300

    wip

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import ImageUploader from "react-images-upload";
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
-      <ImageUploader withIcon={false}
-                     withPreview={true}
-                     label=""
-                     buttonText="Upload Images"
-                     onChange={this.onDrop}
-                     imgExtension={[".jpg", ".gif", ".png", ".gif", ".svg"]}
-                     maxFileSize={1048576}
-                     fileSizeError=" file size is too big" />
     </>
   )
 }
