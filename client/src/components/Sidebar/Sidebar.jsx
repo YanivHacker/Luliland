373393d49c7d4b5f2d171c8ee89cadbcdc75b66a
Jonathan-Langer <jona.lan03@cs.colman.ac.il>
2022-06-29 18:34:32 +0300

    wip

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,48 @@
 import "./sidebar.css"
+import HomeIcon from '@mui/icons-material/Home';
+import {
+    RssFeed,
+    Chat,
+    PlayCircleFilledOutlined,
+    Group,
+    Bookmark,
+    HelpOutline,
+    WorkOutline,
+    Event
+} from "@material-ui/icons";
+
+const SidebarData = [
+    {
+        title: "Feed",
+        icon: <RssFeed />,
+        link: "/home"
+    },
+    {
+        title: "Chat",
+        icon: <Chat />,
+        link: "/home"
+    }
+]
+
+
 
 export default function Sidebar() {
-    
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    {SidebarData.map((val,key)=>{
+                        return (
+                            <li className="sidebarListItem"
+                                key={key}
+                                onClick={() => {window.location.pathname = val.link}}>
+                                <div className="sidebarIcon">{val.icon}</div>
+                                <div className="sidebarListItemTitle">{val.title}</div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </div>
+    )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./home.css"
 import Topbar from "../../components/Topbar/Topbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
-      <Topbar />
+      <>
+        <Topbar />
+        <div className="homeContainer">
+          <Sidebar />
+        </div>
+      </>
   )
 }
