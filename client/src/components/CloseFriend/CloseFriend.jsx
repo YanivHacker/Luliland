976e36687e7fea8793717bca813e2d53c75e8fe2
Jonathan-Langer <jona.lan03@cs.colman.ac.il>
2022-07-19 10:07:04 +0300

    Fixed profile pictures in client side

--- client/src/components/CloseFriend/CloseFriend.jsx
+++ client/src/components/CloseFriend/CloseFriend.jsx
@@ -4,7 +4,7 @@ export default function CloseFriend({user}) {
     console.log(user)
     return (
         <li className="sidebarFriend">
-            <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+            <img className="sidebarFriendImg" src={user.profilePicture ? user.profilePicture : "assets/person/person-icon.png"} alt="" />
             <span className="sidebarFriendName">{user.firstName + " " + user.lastName}</span>
         </li>
     );
